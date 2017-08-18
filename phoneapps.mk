85f8b2dc97ea68a645b8c830e549392fe9ba5bdd
warrenlobo <warrenlobo360@gmail.com>
2017-08-18 21:12:11 +0530

    Final touchups

---
+++ phoneapps.mk
@@ -0,0 +1,43 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := \
+	AntHalService \
+	BasicDreams \
+	Bluetooth \
+	BluetoothMidiService \
+	CertInstaller \
+	Galaxy4 \
+	HoloSpiralWallpaper \
+	KeyChain \
+	LiveWallpapers \
+	NoiseField \
+	PacProcessor \
+	PhaseBeam \
+	PhotoTable \
+	PrintSpooler \
+	LatinIME \
+	Stk \
+	telresources \
+	ThemeManager \
+	TimeService \
+	WAPPushManager
+
+private-phone-priv-apps := \
+	AudioFX \
+	BackupRestoreConfirmation \
+	CellBroadcastReceiver \
+	CMSettingsProvider \
+	CMAudioService \
+	ThemeManagerService \
+	ExternalStorageProvider \
+	FusedLocation \
+	InputDevices \
+	ProxyHandler \
+	SharedStorageBackup \
+	Shell \
+	Telecom \
+	TelephonyProvider \
+	StatementService \
+	TeleService \
+	ThemesProvider
